5fc57e9ab483bdfc7fb5b420a77662a25f3b4bc9
sonu <sonukumar851216@gmail.com>
2024-11-28 00:22:37 +0530

    add endpoint

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express= require('express');
 const app = express();
 app.get('/',(req,res)=>{
-    res.send('Hello World!');
+    res.json({
+        name:'sonu',
+        age:20
+    })
 })
 
 app.get('/sonu',(req,res)=>{
